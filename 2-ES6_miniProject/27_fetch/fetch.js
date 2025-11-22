b84d67e7ce25d659ab42cc865dea10bd7ee52003
Chris Shuo <55964946+chrisshuo123@users.noreply.github.com>
2025-11-22 15:43:50 +0700

    Delete 2-ES6_miniProject directory
    delete unused directory due to annoying repeated 'git pull' request issues after adding new folder directory in my vscode to seperate between javascript miniprojects, php miniprojects, and ES6 miniprojects.

--- 2-ES6_miniProject/27_fetch/fetch.js
+++
@@ -1,6 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/comments', {
-    method: "POST",
-    body: JSON.stringify({})
-})
-.then(response => response.json())
-.then(data => console.log(data))
--- 2-ES6_miniProject/5_exerciseUlangPart1-3/Exercise_1_ulangDasar1-3/soal2_destructuringArrays.js
+++
@@ -1,7 +0,0 @@
-// Diberikan array buah berisi "Apel", "Mangga", dan "Jeruk". Gunakan destructuring array untuk mengambil nilai "Mangga" ke variabel buahKedua.
-
-const buah = ["Apel", "Mangga", "Jeruk"];
-
-// destructuring array
-const [, buahKedua] = buah;
-console.log(buahKedua);
